7415515e085f6cebb5e991db172952863608d7c2
Rederik1104 <116882326+Rederik1104@users.noreply.github.com>
2024-07-03 12:07:44 +0200

    Add files via upload

--- Kalender/newsletter.js
+++ Kalender/newsletter.js
@@ -1,4 +1,5 @@
 async function anmeldung() {
+  var eqaulEmails = 0;
   // Formulardaten für Teilnehmer
   const formDataTeilnehmer = new FormData();
   formDataTeilnehmer.append("name", document.querySelector("#name").value);
@@ -6,19 +7,32 @@ async function anmeldung() {
 
   // Fetch-Anfrage für Teilnehmerdaten
   try {
-    const responseTeilnehmer = await fetch("teilnehmer.php", {
-      method: "POST",
-      body: formDataTeilnehmer,
+    const teilnehmer = await fetch("getAllTeilnehmer.php");
+    const AllEmails = await teilnehmer.json();
+
+    AllEmails.forEach((email) => {
+      if (email == document.querySelector("#email").value) {
+        eqaulEmails = +1;
+      }
     });
 
-    if (!responseTeilnehmer.ok) {
-      throw new Error(
-        "Fehler beim Hinzufügen von Teilnehmerdaten: " +
-          responseTeilnehmer.status
-      );
-    }
+    if (eqaulEmails != 0) {
+      alert("Die Email gibt es schon");
+    } else {
+      const responseTeilnehmer = await fetch("teilnehmer.php", {
+        method: "POST",
+        body: formDataTeilnehmer,
+      });
 
-    console.log("Teilnehmerdaten erfolgreich gesendet.");
+      if (!responseTeilnehmer.ok) {
+        throw new Error(
+          "Fehler beim Hinzufügen von Teilnehmerdaten: " +
+            responseTeilnehmer.status
+        );
+      }
+      console.log("Teilnehmerdaten erfolgreich gesendet.");
+      window.location.href = "index.php";
+    }
   } catch (error) {
     console.error("Fehler beim Senden der Teilnehmerdaten:", error);
     return; // Hier kannst du die Funktion abbrechen oder weitere Maßnahmen ergreifen
